9de3a04364fdb14e0dcee9333dd5c3746f04ba12
Rostyk1206 <rostykvoznyak@gmail.com>
2020-12-07 16:17:49 +0200

    Lesson21Homework

---
+++ Lesson21Homework/Lesson21Homework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson21Homework
+{
+    class Program
+    {
+
+        static void Factorial(int number)
+        {
+            if (number==0)
+            {
+                Console.WriteLine("Factorial 0!=1");
+            }
+            else
+            {
+                Thread t = new Thread(() => Factorial(number)); 
+                t.Start(number-1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Factorial(5);
+        }
+    }
+}
